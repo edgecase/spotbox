85ad50a654a4fa7f1461a509cd3c05cd0f8d0659
Andrew Grieser <agrieser@gmail.com>
2012-02-26 11:27:04 -0500

    Clear search results

--- app/assets/javascripts/views/search_results.js
+++ app/assets/javascripts/views/search_results.js
@@ -1,6 +1,9 @@
 Spotbox.Views.SearchResults = Ember.View.extend({
   templateName: "search_results",
   contentBinding: "Spotbox.Controllers.Search.content",
+  clear: function() {
+    Spotbox.Controllers.Search.set("content", []);
+  },
   showContent: function() {
     return Spotbox.Controllers.Search.get("content").length > 0;;
   }.property("Spotbox.Controllers.Search.content")
