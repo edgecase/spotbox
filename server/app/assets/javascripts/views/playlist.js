c21ddd86d7b1aada72bbfa8701815dbed998151a
Alex Burkhart + Tony Schneider <tonywok+ab+ts@gmail.com>
2012-03-20 16:49:07 -0400

    WIP: playlist view

--- server/app/assets/javascripts/views/playlist.js
+++ server/app/assets/javascripts/views/playlist.js
@@ -1,16 +1,17 @@
 Spotbox.Views.Playlist = Ember.View.extend({
   templateName: "playlist",
-  tagName: "li",
-  currentUriBinding: "Spotbox.Controllers.Playlists.currentUri",
-  didInsertElement: function() {
-    this.setActive();
-  },
-  click: function(event) {
-    Spotbox.Controllers.Playlists.changePlaylist(this.get("model").href);
-  },
-  setActive: function() {
-    var active = this.get("model").href === this.get("currentUri");
-    this.set("active", active);
-    this.set("label", active);
-  }.observes("currentUri")
+  tagName: "table",
+  classNames: ["table", "table-striped", "search_results", "tab-pane"],
+  // currentUriBinding: "Spotbox.Controllers.Playlists.currentUri",
+  // didInsertElement: function() {
+  //   this.setActive();
+  // },
+  // click: function(event) {
+  //   Spotbox.Controllers.Playlists.changePlaylist(this.get("model").href);
+  // },
+  // setActive: function() {
+  //   var active = this.get("model").href === this.get("currentUri");
+  //   this.set("active", active);
+  //   this.set("label", active);
+  // }.observes("currentUri")
 });
