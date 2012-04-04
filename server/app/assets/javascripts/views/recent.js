f40a63cad1ff9902f30ff5326bc9b703afd58e79
Tony Schneider <tonywok@gmail.com>
2012-04-04 00:05:51 -0400

    Add some style to the recent tab

--- server/app/assets/javascripts/views/recent.js
+++ server/app/assets/javascripts/views/recent.js
@@ -1,5 +1,7 @@
 Spotbox.Views.Recent = Ember.View.extend({
   templateName: "recent",
+  tagName: "table",
+  classNames: ["table", "table-striped", "track-list"],
 
   contentBinding: "Spotbox.Controllers.Recent.content",
 
