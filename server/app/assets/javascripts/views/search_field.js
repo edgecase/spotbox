d3af4ba3c06f8483f68a39ec68cbc759e1ee7ac0
Tony Schneider <tonywok@gmail.com>
2012-04-05 22:58:50 -0400

    Reposition and style search field.

--- server/app/assets/javascripts/views/search_field.js
+++ server/app/assets/javascripts/views/search_field.js
@@ -1,11 +1,25 @@
-Spotbox.Views.SearchField = Spotbox.Views.TextField.extend({
+Spotbox.Views.SearchField = Ember.TextField.extend({
+  classNames: ["search-query"],
+  placeholder: "Search",
+  modelBinding: "Spotbox.Controllers.Search.searchModel",
+  valueBinding: "model.query",
+  attributeBindings: ["disabled"],
+
+  disabled: function() {
+    return Spotbox.Controllers.Search.searching;
+  }.property("Spotbox.Controllers.Search.searching"),
 
   focusIn: function() {
     $('#search-tab').tab('show');
   },
 
-  click: function() {
-    this.set("value", "");
+  submit: function(event) {
+    Spotbox.Controllers.Search.search();
+  },
+
+  insertNewline: function(event) {
+    event.preventDefault();
+    this.submit(function() {});
   }
 
 });
