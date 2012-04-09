f594df06a6cb79080c04e65434ae83e1baffd9b4
Tony Schneider <tonywok@gmail.com>
2012-04-09 00:04:26 -0400

    Don't show recent table headers when empty

--- server/app/assets/javascripts/views/recent.js
+++ server/app/assets/javascripts/views/recent.js
@@ -2,5 +2,9 @@ Spotbox.Views.Recent = Ember.View.extend({
   templateName: "recent",
   tagName: "table",
   classNames: ["table", "table-striped", "track-list"],
-  contentBinding: "Spotbox.Controllers.PlayedTracks.content"
+  contentBinding: "Spotbox.Controllers.PlayedTracks.content",
+
+  showContent: function() {
+    return this.get("content").length > 0;
+  }.property("content")
 });
