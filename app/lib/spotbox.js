b52520a4c2df0c24a734907f8ae257710d77dbe3
Andrew Grieser <agrieser@gmail.com>
2012-05-16 20:25:01 -0400

    Add http user agent for api compliance

--- app/lib/spotbox.js
+++ app/lib/spotbox.js
@@ -4,14 +4,6 @@ Spotbox.namespace = function(str) {
   return "spotbox:" + str;
 };
 
-Spotbox.parse_message = function(msg) {
-  var full_message = msg.toString().split("::");
-
-  return {
-      destination : full_message[0]
-    , method      : full_message[1]
-    , args        : full_message.slice(2)
-  }
-};
+Spotbox.userAgent = "spotbox/2.0.alpha1 ( https://github.com/edgecase/spotbox )";
 
 module.exports = Spotbox;
