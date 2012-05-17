2aea7901556f216707400d57400d6a1a66a04bb1
Andrew Grieser <agrieser@gmail.com>
2012-05-17 17:40:24 -0400

    Separate runner from applescript

--- app/lib/applescript.js
+++ app/lib/applescript.js
@@ -1,33 +1,13 @@
-var path          = require("path");
-var fs            = require("fs");
-var underscore    = require("underscore");
-var child_process = require("child_process");
-var appConfig     = require(path.join(__dirname, "..", "..", "config", "app"));
-
-function shell_out(command, args, hollaback) {
-  var child = child_process.spawn(command, args);
-  var result = "";
-  var error = ""
-  child.stdout.on("data", function(chunk) {
-    result += chunk.toString();
-  });
-  child.stderr.on("data", function(chunk) {
-    error += chunk.toString();
-  });
-  child.on("exit", function(code) {
-    if (error) {
-      hollaback(error);
-    } else {
-      hollaback(null, result);
-    }
-  });
-  return child;
-};
+var path       = require("path");
+var fs         = require("fs");
+var underscore = require("underscore");
+var app        = require(path.join(__dirname, "..", "..", "config", "app"));
+var Runner     = require(path.join(app.root, "app", "lib", "runner"));
 
 var Applescript = function() {};
 
 Applescript.run = function(applescriptString, hollaback) {
-  var child = shell_out("osascript", ["-"], function(error, result) {
+  var child = Runner.exec("osascript", ["-"], function(error, result) {
     if (error) {
       console.error("applescript error executing:", applescriptString);
       console.error(error);
---
+++ app/lib/runner.js
@@ -0,0 +1,25 @@
+var child_process = require("child_process");
+
+var Runner = function() {};
+
+Runner.exec = function(command, args, hollaback) {
+  var child = child_process.spawn(command, args);
+  var result = "";
+  var error = ""
+  child.stdout.on("data", function(chunk) {
+    result += chunk.toString();
+  });
+  child.stderr.on("data", function(chunk) {
+    error += chunk.toString();
+  });
+  child.on("exit", function(code) {
+    if (error) {
+      hollaback(error);
+    } else {
+      hollaback(null, result);
+    }
+  });
+  return child;
+};
+
+module.exports = Runner;
