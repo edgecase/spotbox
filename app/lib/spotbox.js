9547f5794eb078e4b1cf76d1aaa86df3abfcafc0
Andrew Grieser <agrieser@gmail.com>
2012-05-17 17:46:16 -0400

    Add version to user agent

--- app/lib/spotbox.js
+++ app/lib/spotbox.js
@@ -1,9 +1,16 @@
+var path    = require("path");
+var fs      = require("fs");
+var app     = require(path.join(__dirname, "..", "..", "config", "app"));
+var package = require(path.join(app.root, "package.json"));
+
 var Spotbox = function() {};
 
 Spotbox.namespace = function(str) {
   return "spotbox:" + str;
 };
 
-Spotbox.userAgent = "spotbox/2.0.alpha1 ( https://github.com/edgecase/spotbox )";
+Spotbox.version = package.version;
+
+Spotbox.userAgent = "spotbox/" + Spotbox.version + " ( https://github.com/edgecase/spotbox )";
 
 module.exports = Spotbox;
